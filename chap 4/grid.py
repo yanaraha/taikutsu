c02ab60c200c206c78171a1a13f9cb34c9256c48
sarai <yanarahait@gmail.com>
2020-09-20 17:30:41 +0900

    Chapter4 Finish

---
+++ chap 4/grid.py
@@ -0,0 +1,17 @@
+def print_grid(grid):
+    for i in range(len(grid[0])):
+        for j in range(len(grid)):
+            print(grid[j][i], end='')
+        print(end='\n')
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+print_grid(grid)
